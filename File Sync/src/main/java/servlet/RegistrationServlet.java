615d537a9de4a98ac9b51aed230c582eaa146008
developer-- <jemo.mgebrishvili@gmail.com>
2017-04-27 19:27:13 +0400

    added registration.jsp

---
+++ File Sync/src/main/java/servlet/RegistrationServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by root on 4/27/17.
+ */
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/registration.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
